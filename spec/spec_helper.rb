aefbf72d5dfb3b59adc727802aec43f9b9047d20
Carlos Paramio <hola@carlosparamio.com>
2014-05-18 23:25:25 +0200

    Include rspec options file, include HTML coverage formatter, correct spec_helper load order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 require "coveralls"
 require "simplecov"
 
-require "lita-ey-info"
-require "lita/rspec"
-
-Coveralls.wear!
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start { add_filter "/spec/" }
+
+require "lita-ey-info"
+require "lita/rspec"
