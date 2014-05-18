665735e31b1b1404594c6658757ac293c7ec4d4c
Carlos Paramio <hola@carlosparamio.com>
2014-05-18 15:01:41 +0200

    Include Travis / Coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
+require "coveralls"
 require "simplecov"
+
+require "lita-ey-info"
+require "lita/rspec"
+
+Coveralls.wear!
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
 ]
 SimpleCov.start { add_filter "/spec/" }
-
-require "lita-ey-info"
-require "lita/rspec"
